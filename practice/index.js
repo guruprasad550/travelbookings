32521aceab967b11fd42395450b7eb73cad98f67
Tlakshmanna9381 <t.lakshmanna2003@gmail.com>
2023-12-10 12:04:05 +0530

    some changes

---
+++ practice/index.js
@@ -0,0 +1,60 @@
+// let str = "Veda Institute Of Technologies";
+// console.log(str.indexOf("Of"));
+
+// const str1 = "Hello, World!";
+// const indexOfHello = str1.indexOf("Hello");
+// console.log(indexOfHello); // Output: 0
+
+// const sentence = "The quick brown fox jumps over jumps the lazy dog";
+// const indexOfFox = sentence.indexOf("l");
+// console.log(indexOfFox); // Output: 16
+
+// const str2 = "Hello, world!";
+// const indexOfWorld = str2.indexOf("World");
+// console.log(indexOfWorld); // Output: -1 (not found, as it's case-sensitive)
+
+// const str3 = "Hello, World, World!";
+// const indexOfWorld1 = str.indexOf("World");
+// const indexOfWorldAfterFirst = str3.indexOf("World", indexOfWorld + 1);
+// // console.log(indexOfWorldAfterFirst); // Output: 13 (index of the second "World")
+
+// let num1 = 5;
+// let num2 = 6;
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1/num2);
+// console.log(num1*num2);
+// console.log(num1%num2);
+
+// //a++
+// let a = 8;
+// console.log(a++);
+// console.log(++a);
+
+// console.log(a--);
+// console.log(--a);
+
+// assignment operatores
+let A = 5;
+console.log(A + 3.123);
+console.log((A -= 3.9087));
+console.log((A /= 3));
+console.log(A * 3);
+
+// comparison operators
+// == (equal)
+// != (not equal)
+// === (strict equal)
+// < (lessthan)
+// > (greater than)
+// >= (greater than or equal)
+// <= (lessthan or equal)
+
+// Examples
+
+let a = 5;
+let b = 8;
+console.log(a > b);
+console.log(a == b);
+console.log(a === b);
+console.log(a < b);
